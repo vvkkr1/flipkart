ede1b9ae6436e92f8fd5c265b9f2576aec9f69a9
vvkkr1 <vivek.kumarr1107@gmail.com>
2023-11-16 16:45:25 +0530

    products-created

--- src/_app.js
+++ src/_app.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
 function HomeApp() {
   return (
     <div>
       {/* This is main react page where parent component rendered */}
       <h1>This is parent pages</h1>
+      <div>
+
+      </div>
     </div>
   )
 }
---
+++ src/components/common/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+        This is Footer component
+    </div>
+  )
+}
+
+export default Footer
