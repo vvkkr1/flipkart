9d4db301018441c0cccfd5aa5a0bbdc6083a3dfd
vvkkr1 <vivek.kumarr1107@gmail.com>
2024-01-04 21:55:53 +0530

    styling-login-btn

--- src/components/tabOptions/ItemList/index.js
+++ src/components/tabOptions/ItemList/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import './itemList.css'
 
+//Todo 
+//! not to do
+//? to write question
+//* important notes
+
 const ItemList = ({list}) => {
   return (
     <div className='item-list'>
         <div className='item-list-wrap max-width2'>
             <div className='item-list-container'>
                 <div className='item-image-container absolute-center' style={{display:"flex",justifyContent:"space-evenly"}}>
-                  <img src={list.img}
+                  <a href="www.google.com">
+                    <img src={list.img}
                        className='item-img'
                        alt={list.title}
                        style={{height:"64px",width:"64px"}}
-                     />
+                    />
+                  </a>
                 </div>
                 <div className='item-list-title absolute-center'>{list.title}</div>
             </div>
